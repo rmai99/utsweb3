74ea41278baa21b4a5d4acb83f1d9d060413fd21
rmai99 <maidakrismarajagukguk@gmail.com>
2019-04-11 09:04:07 +0700

    Updates

---
+++ precache-manifest.b2931b176818419da597bbfd63ce86b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/utsweb3/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "371dd3676839bd68e7ae",
+    "url": "/utsweb3/static/js/runtime~main.371dd367.js"
+  },
+  {
+    "revision": "339643f5b3cd642a8e0f",
+    "url": "/utsweb3/static/js/main.339643f5.chunk.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/utsweb3/static/js/2.681b5105.chunk.js"
+  },
+  {
+    "revision": "339643f5b3cd642a8e0f",
+    "url": "/utsweb3/static/css/main.77222547.chunk.css"
+  },
+  {
+    "revision": "b3b34969b044614ef46965b98545e433",
+    "url": "/utsweb3/index.html"
+  }
+];
